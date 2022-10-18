33d7cac13c5de592ff010a67da3ecbfb73ce2b4d
Ivan Molinov <ivan.molinov@mail.ru>
2022-10-18 20:04:51 +0300

    Задача 2

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 2
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+//      Console.WriteLine(a);
+// else
+//     Console.WriteLine(b);
+
