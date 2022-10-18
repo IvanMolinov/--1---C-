27345a0a6535544df4026709e4ea1eef56539d66
Ivan Molinov <ivan.molinov@mail.ru>
2022-10-18 20:15:02 +0300

    Задача 2 окончательно

--- Program.cs
+++ Program.cs
@@ -2,7 +2,11 @@
 // int a = Convert.ToInt32(Console.ReadLine());
 // int b = Convert.ToInt32(Console.ReadLine());
 // if (a > b)
-//      Console.WriteLine(a);
-// else
+//     Console.WriteLine(a);
+// else if (a < b)
 //     Console.WriteLine(b);
+// else Console.WriteLine("Числа равны");
+
+
+
 
